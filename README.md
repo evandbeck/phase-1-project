62fb34ac3de531fdd7f9a3ac7e0308951796c7f0
Evan Beck <evandrydenbeck@gmail.com>
2022-01-25 12:30:29 -0500

    added sweatshirt list to DOM

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Phase 1 Project
 
 ## Sweatshirt Website Summary
-01/25/22 - Basic HTML & CSS
+01/25/22 
+- Basic HTML & CSS
+- db.json file started
--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-console.log("Hi!")
+// Endpoints
+const sweatshirts = "http://localhost:3000/Sweatshirts"
+
+// DOM Elements
+const sweatshirtList = document.querySelector('#list')
+const sweatshirtInfo = document.querySelecotr('#info')
+
+// Fetch
+function fetchSweatshirts () {
+    fetch(sweatshirts)
+    .then(response => response.json())
+    .then(sweatshirtArray => iterateArray(sweatshirtArray))
+}
+
+// Display Sweatshirt Names on DOM
+function iterateArray (sweatshirtArray) {
+    sweatshirtArray.forEach(sweatshirtObj => {
+        const p = document.createElement('p')
+        p.innerText = sweatshirtObj.name
+        // Add Event Listener for Click
+        p.addEventListener('click', function(e) {
+            //Name
+            const h2 = document.createElement('h2')
+            h2.innerText = sweatshirtObj.name
+            sweatshirtInfo.append(h2)
+            //Brand
+            const h3 = document.createElement('h3')
+            h3.innerText = sweatshirtObj.brand
+            sweatshirtInfo.append(h3)
+            //Image
+            const img = document.createElement('img')
+            img.src = sweatshirtObj.image
+            img.alt = sweatshirtObj.name
+            sweatshirtInfo.append(img)
+            //Description
+            const desc = document.createElement('p')
+            desc.innerText = sweatshirtObj.description
+            sweatshirtInfo.append(desc)
+        },
+        sweatshirtList.append(p)
+    })
+}
+
+
+// On Page Load Event
+document.addEventListener('DOMContentLoaded', fetchSweatshirts)
