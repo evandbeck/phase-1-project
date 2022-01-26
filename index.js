80edac6cf9a1b7f69050f57489560f32670356c1
Evan Beck <evandrydenbeck@gmail.com>
2022-01-26 13:34:38 -0500

    refactoring

--- index.js
+++ index.js
@@ -16,35 +16,36 @@ const commentList = document.querySelector('#commentList')
 
 // Display Sweatshirt Names on DOM
 function iterateArray (sweatshirtArray) {
-    sweatshirtArray.forEach(sweatshirtObj => {
-        const p = document.createElement('p')
-        p.innerText = sweatshirtObj.name
-        // Add Event Listener for Click
-        p.addEventListener('click', () => {
-            //Name
-            const h2 = document.querySelector('#name')
-            h2.innerText = sweatshirtObj.name
-            sweatshirtInfo.append(h2)
-            //Brand
-            const h3 = document.querySelector('#brand')
-            h3.innerText = sweatshirtObj.brand
-            sweatshirtInfo.append(h3)
-            //Image
-            const img = document.querySelector('#image')
-            img.src = sweatshirtObj.image
-            img.alt = sweatshirtObj.name
-            sweatshirtInfo.append(img)
-            //Description
-            const desc = document.querySelector('#description')
-            desc.innerText = sweatshirtObj.description
-            sweatshirtInfo.append(desc)
-            // Buy Now Button Quantity
-            quantity.innerText = sweatshirtObj.quantity
-            // Like Total
-            totalLikes.innerText = sweatshirtObj.likes
-        },
-        sweatshirtList.append(p)
-        )}
+    sweatshirtArray.forEach(sweatshirtObj => renderSweatshirts(sweatshirtObj))}
+
+function renderSweatshirts (sweatshirtObj) {
+    const p = document.createElement('p')
+    p.innerText = sweatshirtObj.name
+    // Add Event Listener for Click
+    p.addEventListener('click', () => {
+        //Name
+        const h2 = document.querySelector('#name')
+        h2.innerText = sweatshirtObj.name
+        sweatshirtInfo.append(h2)
+        //Brand
+        const h3 = document.querySelector('#brand')
+        h3.innerText = sweatshirtObj.brand
+        sweatshirtInfo.append(h3)
+        //Image
+        const img = document.querySelector('#image')
+        img.src = sweatshirtObj.image
+        img.alt = sweatshirtObj.name
+        sweatshirtInfo.append(img)
+        //Description
+        const desc = document.querySelector('#description')
+        desc.innerText = sweatshirtObj.description
+        sweatshirtInfo.append(desc)
+        // Buy Now Button Quantity
+        quantity.innerText = sweatshirtObj.quantity
+        // Like Total
+        totalLikes.innerText = sweatshirtObj.likes
+    },
+    sweatshirtList.append(p)
     )}
 
 // Buy Now Button
